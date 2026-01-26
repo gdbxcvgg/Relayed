06d2efd03b4bee069b1e959298adb466fad33e79
Dominik Koziatek <harrty.dev@gmail.com>
2026-01-26 19:31:09 +0100

    feat(db): implement custom User model

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,1 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    model = User
+
+    list_display = ['id', 'username', 'email']
+    list_filter = []
+
+    search_fields = ['username', 'display_name', 'email']
+
+    ordering = ['-id']
+
+    readonly_fields = ['id', 'date_joined', 'last_login']
+
+    fieldsets = [
+        ['Basic Info', {
+            'fields': ['id', 'username', 'email', 'display_name', 'date_of_birth', 'avatar'],
+        }],
+        ['Authentication', {
+            'fields': ['password', 'last_login'],
+        }],
+        ['Permissions', {
+            'fields': ['is_active', 'is_staff', 'is_superuser'],
+        }],
+        ['Other', {
+            'fields': ['date_joined']
+        }]
+    ]
+
+    add_fieldsets = [
+        ['Basic Info', {
+            'fields': ['email', 'username', 'display_name', 'date_of_birth'],
+        }],
+        ['Authentication', {
+            'fields': ['password1', 'password2'],
+        }],
+        ['Permissions', {
+            'fields': ['is_active', 'is_staff', 'is_superuser'],
+        }],
+    ]
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,2 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core import validators
+import uuid
+
+
+class UsernameCharactersValidator(validators.RegexValidator):
+    "Accept only letters, numbers, underscores(_) and dots(.)"
+    
+    regex = r"^[a-zA-Z0-9_.]+$"
+
+
+class User(AbstractUser):
+    username_validator = UsernameCharactersValidator()
+
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid7)
+
+    username = models.CharField(max_length=64, unique=True, validators=[username_validator, validators.MinLengthValidator(2)])
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    display_name = models.CharField(max_length=100, null=True, blank=True)
+    avatar = models.CharField(max_length=200, null=True, blank=True)
+    
+    first_name = None
+    last_name = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def clean(self):
+        super().clean()
+        self.username = self.username.lower()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+
+    def get_full_name(self):
+        if self.display_name:
+            return self.display_name
+        return self.username
+
+    get_short_name = get_full_name
 
