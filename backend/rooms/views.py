75f9bcb4a7dfc86f2831ed4cc6d7eb15502883f0
Dominik Koziatek <harrty.dev@gmail.com>
2026-02-08 00:49:00 +0100

    feat: add permissions for room views

--- backend/rooms/views.py
+++ backend/rooms/views.py
@@ -3,31 +3,20 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.exceptions import PermissionDenied
 from . import models, serializers
+from servers.permissions import IsServerOwnerOrMemberRetrieve
 
 
 class RoomRetrieveUpdateDeleteAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.RoomSerializer
 
+    perm_server_path = 'server'
+    permission_classes = [IsServerOwnerOrMemberRetrieve]
+
     def get_object(self):
-        return get_object_or_404(models.Room, pk=self.kwargs['pk'], is_deleted=False)
-
-    
-    def perform_update(self, serializer):
-        room = self.get_object()
-        server = room.server
-
-        if server and server.owner != self.request.user:
-            raise PermissionDenied
-        
-        serializer.save()
+        obj = get_object_or_404(models.Room, pk=self.kwargs['pk'], is_deleted=False)
+        self.check_object_permissions(self.request, obj)
+        return obj
 
     def perform_destroy(self, instance):
-        room = self.get_object()
-        server = room.server
-        if server and server.owner != self.request.user:
-            raise PermissionDenied
-        
         instance.is_deleted = True
         instance.save()
-        
-
