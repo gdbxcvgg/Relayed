7406db227db54261b85cfaf255a2df1641045bf0
Dominik Koziatek <harrty.dev@gmail.com>
2026-02-18 23:50:44 +0100

    ux/ui: add register page

---
+++ frontend/src/pages/RegisterPage.tsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { Link } from "react-router";
+
+import FormInput from "../components/FormInput";
+
+const RegisterPage = () => {
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [displayName, setDisplayName] = useState("");
+    const [dateOfBirth, setDateOfBirth] = useState("");
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        // [ register logic ]
+    };
+
+    return (
+        <div className="flex flex-row justify-center items-center min-h-dvh sm:bg-[url(./background.png)] bg-fixed bg-cover bg-center">
+            <main className="flex flex-col bg-[#0C0C0C] text-white w-dvh h-dvh sm:w-xl sm:h-225 py-20 px-8 sm:p-15 gap-4 sm:rounded-2xl">
+                <header className="text-center pb-10">
+                    <h1 className="text-3xl font-extrabold">
+                        Create an account
+                    </h1>
+                </header>
+
+                <form onSubmit={handleSubmit} className="flex flex-col gap-12">
+                    <div className="flex flex-col gap-7">
+                        <FormInput
+                            id="email"
+                            type="email"
+                            required
+                            onChange={(e) => setEmail(e.target.value)}
+                            label_text="Email"
+                        />
+
+                        <FormInput
+                            id="username"
+                            type="text"
+                            required
+                            onChange={(e) => setUsername(e.target.value)}
+                            label_text="Username"
+                        />
+
+                        <FormInput
+                            id="display_name"
+                            type="text"
+                            onChange={(e) => setDisplayName(e.target.value)}
+                            label_text="Display Name"
+                        />
+
+                        <FormInput
+                            id="password"
+                            type="password"
+                            required
+                            onChange={(e) => setPassword(e.target.value)}
+                            label_text="Password"
+                        />
+
+                        <FormInput
+                            id="date_of_birth"
+                            type="date"
+                            required
+                            onChange={(e) => setDateOfBirth(e.target.value)}
+                            label_text="Date of Birth"
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="bg-[#785D94] rounded-lg p-2 h-12 font-extrabold hover:bg-[#55406b] hover:cursor-pointer"
+                    >
+                        Register
+                    </button>
+                </form>
+                <p className="flex flex-row gap-3">
+                    Already have an account?
+                    <Link to="/login" className="text-[#785D94] font-bold">
+                        Log in
+                    </Link>
+                </p>
+            </main>
+        </div>
+    );
+};
+
+export default RegisterPage;
--- frontend/src/routers/MainRouter.tsx
+++ frontend/src/routers/MainRouter.tsx
@@ -1,17 +1,18 @@
-import { Route, Routes, BrowserRouter } from 'react-router'
-import HomePage from '../pages/HomePage'
-import LoginPage from '../pages/LoginPage'
-
+import { Route, Routes, BrowserRouter } from "react-router";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
 
 const MainRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route index element={<HomePage />} />
-                <Route path='login' element={<LoginPage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="register" element={<RegisterPage />} />
             </Routes>
         </BrowserRouter>
-    )
-}
+    );
+};
 
-export default MainRouter
+export default MainRouter;
