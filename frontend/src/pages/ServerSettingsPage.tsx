1f304b68b1d0d5618408a37da352f29968a66651
Dominik Koziatek <harrty.dev@gmail.com>
2026-03-14 20:44:50 +0100

    ui/ux: add server settings sidebar

--- frontend/src/pages/ServerSettingsPage.tsx
+++ frontend/src/pages/ServerSettingsPage.tsx
@@ -1,7 +1,41 @@
 import { useNavigate, useParams } from "react-router";
 import useServer from "../hooks/useServer";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useUser from "../hooks/useUser";
+import api from "../services/api";
+
+type CurrentMenuType = "profile" | "members" | "roles" | "invites" | "bans";
+
+const MenuItem = ({
+    children,
+    onClick,
+    currentMenu,
+    id,
+}: {
+    children: React.ReactNode;
+    onClick?: () => void;
+    currentMenu: string;
+    id?: CurrentMenuType;
+}) => {
+    return (
+        <div className="hover:bg-(--bg-main) hover:cursor-pointer rounded-md font-semibold">
+            <div
+                className={
+                    currentMenu === id
+                        ? "bg-[#292929] p-2 rounded-md"
+                        : "p-2 rounded-md"
+                }
+                onClick={onClick}
+            >
+                <div className="flex gap-4 items-center">{children}</div>
+            </div>
+        </div>
+    );
+};
+
+const MenuDivisor = () => {
+    return <div className="w-full h-0.25 min-h-0.25 bg-[#323232]"></div>;
+};
 
 const ServerSettingsPage = () => {
     const { serverId } = useParams();
@@ -9,6 +43,8 @@ const ServerSettingsPage = () => {
     const { user } = useUser();
 
     const navigate = useNavigate();
+
+    const [currentMenu, setCurrentMenu] = useState<CurrentMenuType>("profile");
 
     useEffect(() => {
         if (!serverId) return;
@@ -27,9 +63,61 @@ const ServerSettingsPage = () => {
         if (server.owner.id !== user.id) navigate("/channels/@me");
     }, [server, user]);
 
+    const deleteServer = async () => {
+        if (!server) return;
+        const res = await api.delete(`servers/${server.id}`);
+        if (res.status !== 204) return;
+        navigate("/channels/@me");
+    };
+
     return (
-        <div>
-            <div>{server?.name} settings</div>
+        <div className="flex w-full">
+            <aside className="w-78 min-w-78 border-r border-r-(--border-color) bg-(--bg-secondary) flex flex-col p-4 gap-3 grow overflow-y-auto [&::-webkit-scrollbar]:w-[6px] [&::-webkit-scrollbar-thumb]:bg-gray-400 [&::-webkit-scrollbar-thumb]:rounded-[3px]">
+                <MenuItem
+                    onClick={() => navigate("/app")}
+                    currentMenu={currentMenu}
+                >
+                    <img src="/arrow-left.png" className="w-5 h-5" />
+                    Go Back
+                </MenuItem>
+
+                <MenuDivisor />
+
+                <div className="flex flex-col gap-2">
+                    <MenuItem
+                        onClick={() => {
+                            setCurrentMenu("profile");
+                        }}
+                        currentMenu={currentMenu}
+                        id="profile"
+                    >
+                        <img src="/person.png" className="w-5 h-5" />
+                        Server Profile
+                    </MenuItem>
+
+                    <MenuItem
+                        onClick={() => {
+                            setCurrentMenu("members");
+                        }}
+                        currentMenu={currentMenu}
+                        id="members"
+                    >
+                        <img src="/person.png" className="w-5 h-5" />
+                        Members
+                    </MenuItem>
+                </div>
+
+                <MenuDivisor />
+
+                <MenuItem onClick={deleteServer} currentMenu={currentMenu}>
+                    <img src="/logout.png" className="w-5 h-5" />
+                    <span className="text-[#ca2828]">Delete Server</span>
+                </MenuItem>
+            </aside>
+            <main className="w-full grow overflow-y-auto [&::-webkit-scrollbar]:w-[6px] [&::-webkit-scrollbar-thumb]:bg-gray-400 [&::-webkit-scrollbar-thumb]:rounded-[3px]">
+                {currentMenu === "profile" && <div>server profile</div>}
+                {currentMenu === "members" && <div>server members</div>}
+            </main>
         </div>
     );
 };
