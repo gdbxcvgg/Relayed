dbe2a26e393bbf41946597a7f833695a2ae2cbbe
Dominik Koziatek <harrty.dev@gmail.com>
2026-04-24 20:52:17 +0200

    feat: add ability to delete room

--- frontend/src/components/room/RoomSettingsPopup.tsx
+++ frontend/src/components/room/RoomSettingsPopup.tsx
@@ -19,6 +19,14 @@ const RoomSettingsPopup = ({
         onClose();
     };
 
+    const handleDelete = async () => {
+        const res = await api.delete(`rooms/${room.id}`);
+
+        if (res.status !== 204) return;
+
+        onClose();
+    };
+
     return (
         <div className="flex flex-col gap-12 grow w-100">
             <h1 className="text-lg font-bold">Channel Settings</h1>
@@ -30,13 +38,22 @@ const RoomSettingsPopup = ({
                 onChange={(e) => setRoomName(e.target.value)}
             />
 
-            <button
-                className="p-2 rounded-xl bg-[#785D94] enabled:hover:bg-[#674f80] enabled:active:bg-[#5f4976] enabled:hover:cursor-pointer disabled:bg-[#37234c]"
-                onClick={handleUpdate}
-                disabled={room.name === roomName}
-            >
-                Create
-            </button>
+            <div className="flex flex-col gap-2">
+                <button
+                    className="p-2 rounded-xl bg-[#785D94] enabled:hover:bg-[#674f80] enabled:active:bg-[#5f4976] enabled:hover:cursor-pointer disabled:bg-[#37234c]"
+                    onClick={handleUpdate}
+                    disabled={room.name === roomName}
+                >
+                    Update
+                </button>
+
+                <button
+                    className="p-2 rounded-xl bg-[#913a3a] enabled:hover:bg-[#803535] enabled:active:bg-[#782929] enabled:hover:cursor-pointer"
+                    onClick={handleDelete}
+                >
+                    Delete
+                </button>
+            </div>
         </div>
     );
 };
