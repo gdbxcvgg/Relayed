3fbb3cac2eac94648c9878d0835960112b8dd338
Dominik Koziatek <harrty.dev@gmail.com>
2026-04-29 15:40:41 +0200

    fix(api): add server_id to gateway room event serializer

--- backend/rooms/serializers.py
+++ backend/rooms/serializers.py
@@ -24,10 +24,11 @@ class ParialRoomSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
     parent = serializers.UUIDField(read_only=True, source='parent.id', default=None)
+    server_id = serializers.CharField(read_only=True, source='server.id')
 
     class Meta:
         model = models.Room
-        fields = ['id', 'name', 'description', 'room_type', 'parent', 'created_at']
+        fields = ['id', 'name', 'description', 'room_type', 'parent', 'created_at', 'server_id']
 
     
 class RoomDeletedSerializer(serializers.ModelSerializer):
