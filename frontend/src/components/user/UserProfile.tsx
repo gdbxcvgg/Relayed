ef503ca0d120ac8b21a79b7941da3610ecff6390
Dominik Koziatek <harrty.dev@gmail.com>
2026-05-16 19:56:49 +0200

    fix: make DM message button on user profile functional

--- frontend/src/components/user/UserProfile.tsx
+++ frontend/src/components/user/UserProfile.tsx
@@ -2,9 +2,16 @@ import { useEffect, useState } from "react";
 import type { UserType } from "../../types/user";
 import api from "../../services/api";
 import UserAvatar from "./UserAvatar";
+import useUser from "../../hooks/useUser";
+import { useNavigate } from "react-router";
+import type { RoomType } from "../../types/room";
 
 const UserProfile = ({ userId }: { userId: string }) => {
     const [user, setUser] = useState<UserType | null>(null);
+
+    const { user: loggedInUser } = useUser();
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -15,6 +22,36 @@ const UserProfile = ({ userId }: { userId: string }) => {
 
         fetchUser();
     }, [userId]);
+
+    const openMessage = async () => {
+        const res = await api.post<RoomType>("users/@me/channels", {
+            recipients: [user?.id],
+        });
+        if (res.status !== 200) return;
+
+        navigate(`/channels/@me/${res.data.id}`);
+    };
+
+    if (!user)
+        return (
+            <div className="flex flex-col mb-10">
+                <div className="w-160 h-24 sm:h-32 flex bg-[#363636] sm:rounded-t-xl pt-12 sm:pt-16 px-6">
+                    <div className="bg-[#121212] rounded-full flex justify-center items-center size-24 sm:size-32"></div>
+                </div>
+                <div className="pt-15 sm:pt-19 px-6 flex flex-col gap-5">
+                    <div className="flex flex-col sm:flex-row justify-between gap-5 w-full">
+                        <div>
+                            <div className="bg-[#121212] w-55 max-w-full shrink h-6 mb-0.75 rounded-md"></div>
+                            <div className="bg-[#121212] w-40 max-w-full shrink h-4 rounded-md"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="bg-[#121212] w-50 max-w-full shrink h-4 mb-0.75 rounded-md"></div>
+                        <div className="bg-[#121212] w-35 max-w-full shrink h-4 rounded-md"></div>
+                    </div>
+                </div>
+            </div>
+        );
 
     return (
         <div className="flex flex-col mb-10">
@@ -30,16 +67,22 @@ const UserProfile = ({ userId }: { userId: string }) => {
                         <div className="text-xs">@ {user?.username}</div>
                     </div>
 
-                    <div className="flex gap-2">
-                        <button className="flex gap-1 items-center text-sm bg-[#45396c] py-1.5 px-3.5 rounded-lg font-semibold hover:cursor-pointer hover:bg-[#3f3463] active:bg-[#3a305a]">
-                            <img src="/add-friend.png" className="size-4" />
-                            <div>Add Friend</div>
-                        </button>
+                    {loggedInUser?.id !== user?.id && (
+                        <div className="flex gap-2">
+                            <button className="flex gap-1 items-center text-sm bg-[#45396c] py-1.5 px-3.5 rounded-lg font-semibold hover:cursor-pointer hover:bg-[#3f3463] active:bg-[#3a305a]">
+                                <img src="/add-friend.png" className="size-4" />
+                                <div>Add Friend</div>
+                            </button>
 
-                        <button className="flex gap-1 items-center text-sm bg-[#3c3c3c] py-3 px-3 rounded-lg font-semibold hover:cursor-pointer hover:bg-[#363636] active:bg-[#2e2e2e]">
-                            <img src="/message.png" className="size-4" />
-                        </button>
-                    </div>
+                            <button className="flex gap-1 items-center text-sm bg-[#3c3c3c] py-3 px-3 rounded-lg font-semibold hover:cursor-pointer hover:bg-[#363636] active:bg-[#2e2e2e]">
+                                <img
+                                    src="/message.png"
+                                    className="size-4"
+                                    onClick={openMessage}
+                                />
+                            </button>
+                        </div>
+                    )}
                 </div>
                 <div>
                     <div className="text-sm">Member Since</div>
